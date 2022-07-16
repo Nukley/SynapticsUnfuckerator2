ee12c6e66c19fecc8474bd7f71033088d34d79c1
Lannuked <minecraftfreak303@gmail.com>
2022-07-16 17:33:12 -0400

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace SynapticsUnfuckerator
+﻿namespace SynapticsUnfuckerator2
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,52 +23,55 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(391, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Fix Synaptics Pls";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 60);
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 73);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(391, 171);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            this.richTextBox1.Size = new System.Drawing.Size(471, 149);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Welcome to synaptics unfuckerator!";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(470, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Fix it pls";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 243);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(495, 234);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Palmcheck Unfuckerator";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Button button1;
-        private RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
+
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,15 @@
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace SynapticsUnfuckerator
+namespace SynapticsUnfuckerator2
 {
     public partial class Form1 : Form
     {
@@ -8,22 +17,27 @@ namespace SynapticsUnfuckerator
         {
             InitializeComponent();
         }
+
+        RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,RegistryView.Registry64);
         private void button1_Click(object sender, EventArgs e)
         {
-            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Synaptics\\SynTP\\Defaults", true);
+            RegistryKey registryKey = hklm.OpenSubKey(@"SOFTWARE\Synaptics\SynTP\Defaults", true);
             foreach (string valueName in registryKey.GetValueNames())
             {
                 if (valueName.ToLower().StartsWith("palmkms"))
                 {
-                    richTextBox1.Text = richTextBox1.Text + "Disabling: SOFTWARE\\Synaptics\\SynTP\\Defaults\\" + valueName + "\n";
-                    Console.WriteLine(valueName + " " + registryKey.GetValue(valueName)?.ToString());
+                    richTextBox1.Text = richTextBox1.Text + "\nDisabling: SOFTWARE\\Synaptics\\SynTP\\Defaults\\" + valueName;
                     registryKey.SetValue(valueName, (object)0);
-                    Console.WriteLine(valueName + " " + registryKey.GetValue(valueName)?.ToString());
                 }
             }
-            RegistryKey registryKey2 = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad", true);
+            RegistryKey registryKey2 = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad", true);
             registryKey2.SetValue("AAPThreshold", (object)0);
-            richTextBox1.Text = richTextBox1.Text + "Disabling: SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\\" + "AAPThreshold" + "\n Done!";
+            richTextBox1.Text = richTextBox1.Text + "\nDisabling: SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\PrecisionTouchPad\\" + "AAPThreshold" + "\n Done!";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
